e61062f2a64d88b6ffbf1aa2db7d4b2526a2597c
LilyStilson <lunamusicoff@gmail.com>
2025-02-13 22:37:57 +0100

    add(mock): added more mock users

--- src/mock/MockUsers.ts
+++ src/mock/MockUsers.ts
@@ -1,10 +1,17 @@
 import Uber from "./Uber.json"
 import Jelosus1 from "./Jelosus1.json"
+import LilyStilson from "./LilyStilson.json"
+import Neijin from "./Neijin.json"
+import Alt from "./Альт.json"
+
 import EnkaResponse from "../enka/api/EnkaResponse"
 
 const Users = [
     Uber as unknown as EnkaResponse, 
     Jelosus1 as unknown as EnkaResponse,
+    LilyStilson as unknown as EnkaResponse,
+    Neijin as unknown as EnkaResponse,
+    Alt as unknown as EnkaResponse
 ]
 
 
