25f330f467570c6688efc800f85e4f2e1b7d65c2
LilyStilson <lunamusicoff@gmail.com>
2025-02-22 01:35:43 +0100

    fix: added cors package to fix cors issues...

--- backend/server.ts
+++ backend/server.ts
@@ -1,26 +1,21 @@
 import Users from "./mock/MockUsers"
 import express from "express"
+import cors from "cors"
 
 const app = express()
 const port = 5100
 
-app.get("/", (_, res) => {
-    if (process.env.NODE_ENV === "development") {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-    }
+app.use(cors({
+    origin: process.env.NODE_ENV === "development" ? "*" : "https://interknot.space"
+}))
 
+app.get("/", (_, res) => {
     res.type("text/plain").send("Inter-knot data server. Version 0.1.0-beta")
 })
 
 app.get("/profile/:uid", (req, res) => {
     const { uid } = req.params
     const user = Users.find(u => u.Uid.toString() === uid)
-
-    if (process.env.NODE_ENV === "dev") {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    }
 
     if (user) {
         res.json(user)
@@ -31,12 +26,6 @@ app.get("/profile/:uid", (req, res) => {
 
 app.get("/profiles", (req, res) => {
     const { query, listAll } = req.query
-
-    if (process.env.NODE_ENV === "dev") {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    }
 
     if (listAll) {
         res.json(Users.map(u => u.Uid))
