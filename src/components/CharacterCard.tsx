43429273de2863f6f430ab074ccd23a6b8e4b688
LilyStilson <lunamusicoff@gmail.com>
2025-02-24 01:51:00 +0100

    impl: added core props, core skill and talents to card; impl: base drive disc display implementation

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,11 +1,14 @@
-import { BackgroundImage, Card, Grid, Group, Image, Stack, Title } from "@mantine/core"
+import { BackgroundImage, Card, Group, Image, SimpleGrid, Stack, Title } from "@mantine/core"
 import { Character } from "../../backend/data/types/Character"
 import "./styles/CharacterCard.css"
-import { ProfessionIcon, ZenlessIcon, getRarityIcon } from "./icons/Icons"
+import { ProfessionIcon, ZenlessIcon, getDriveDiscGradient, getRarityIcon } from "./icons/Icons"
 import * as Mindscapes from "./icons/mindscapes"
+import * as TalentIcons from "./icons/talents"
+import * as CoreSkillIcons from "./icons/core"
 import { Weapon } from "../../backend/data/types/Weapon"
 import { ValueProperty } from "../../backend/data/types/Property"
-import { useEffect, useState } from "react"
+import React from "react"
+import { DriveDisk } from "../../backend/data/types/DriveDisk"
 
 function MindscapeIcons({ level, size }: { level: number, size?: number }): React.ReactElement {
     size = size || 16;
@@ -45,7 +48,7 @@ interface ICharacterNameProps {
 
 function CharacterName({ name, element, profession, level, msLevel }: ICharacterNameProps): React.ReactElement {
     return (
-        <Stack className="cc-name" gap="0">
+        <Stack gap="0">
             <Group gap="4px">
                 <Title order={3}>{name}</Title>
                 <ZenlessIcon elementName={element} size={16} />
@@ -103,6 +106,78 @@ function WeaponEngine({ weapon }: { weapon?: Weapon }): React.ReactElement {
     )
 }
 
+function Stat({ stat }: { stat: ValueProperty }): React.ReactElement {
+    return (
+        <div className="cc-stat">
+            <ZenlessIcon id={stat.Id} size={12} />
+            <Title order={6} fz="9px" ml="4px">{stat.Name}</Title>
+            <Title order={6} fz="9px">{ValueProperty.format(stat.Format, stat.Value, true)}</Title>
+        </div>
+    )
+}
+
+function CoreSkill({ level }: { level: number }): React.ReactElement {
+    const isActive = (lvl: number): string => (lvl <= level) ? "var(--accent)" : "var(--mantine-color-dark-9)"
+    return (
+        <Group className="cc-core" gap="0px" justify="space-between" wrap="nowrap">
+            <div style={{ backgroundColor: isActive(1) }}><CoreSkillIcons.A fill="white" height="12px" /></div>
+            <div style={{ backgroundColor: isActive(2) }}><CoreSkillIcons.B fill="white" height="12px" /></div>
+            <div style={{ backgroundColor: isActive(3) }}><CoreSkillIcons.C fill="white" height="12px" /></div>
+            <div style={{ backgroundColor: isActive(4) }}><CoreSkillIcons.D fill="white" height="12px" /></div>
+            <div style={{ backgroundColor: isActive(5) }}><CoreSkillIcons.E fill="white" height="12px" /></div>
+            <div style={{ backgroundColor: isActive(6) }}><CoreSkillIcons.F fill="white" height="12px" /></div>
+        </Group>
+    )
+}
+
+function Talents({ talentLevels }: { talentLevels: number[] }): React.ReactElement {
+    return (
+        <Group className="cc-talents" gap="4px" justify="center" align="center">
+            <div className="cc-talent">
+                <TalentIcons.NormalAtk width="32px" />
+                <Title fz="6px" order={6} className="cc-talent-level">{talentLevels[0]}</Title>
+            </div>
+            <div className="cc-talent">
+                <TalentIcons.Dodge width="32px" />
+                <Title fz="6px" order={6} className="cc-talent-level">{talentLevels[1]}</Title>
+            </div>
+            <div className="cc-talent">
+                <TalentIcons.Switch width="32px" />
+                <Title fz="6px" order={6} className="cc-talent-level">{talentLevels[2]}</Title>
+            </div>
+            <div className="cc-talent">
+                <TalentIcons.Skill width="32px" />
+                <Title fz="6px" order={6} className="cc-talent-level">{talentLevels[3]}</Title>
+            </div>
+            <div className="cc-talent">
+                <TalentIcons.Ultimate width="32px" />
+                <Title fz="6px" order={6} className="cc-talent-level">{talentLevels[4]}</Title>
+            </div>
+        </Group>
+    )
+}
+
+function DriveDisc({ disc }: { disc: DriveDisk }) {
+    return (
+        <div className="cc-disc" style={{ "--disc-gradient": getDriveDiscGradient(disc.SetId) } as React.CSSProperties}>
+            <Group gap="4px" className="cc-disc-main" wrap="nowrap">
+                <div className="cc-disc-icon">
+                    <Image src={disc.IconUrl} alt={disc.Name} />
+                    <Image src={getRarityIcon(disc.Rarity)} alt={disc.Rarity.toString()} />
+                </div>
+                <Stack gap="0px" justify="space-evenly" style={{ zIndex: "100" }}>
+                    <Title order={6} fz="8px">Lv. {disc.Level}</Title>
+                    <Group gap="2px" wrap="nowrap">
+                        <ZenlessIcon id={disc.MainStat.Id} size="18px"/>
+                        <Title order={6} fz="14px">{ValueProperty.format(disc.MainStat.Format, disc.MainStat.Value, true)}</Title>
+                    </Group>
+                    <Title order={6} fz="8px">CV 0.0</Title>
+                </Stack>
+            </Group>
+        </div>
+    )
+}
+
 export default function CharacterCard({ uid, username, character }: ICharacterCardProps): React.ReactElement {
     return (
         <Card className="character-card" withBorder shadow="xs" m="lg" p="0px"
@@ -111,16 +186,33 @@ export default function CharacterCard({ uid, username, character }: ICharacterCa
                 <div className="cc-image">
                     <BackgroundImage mt="xs" className="character-img" src={character.ImageUrl} />
                 </div>
-                <div className="cc-stats-bg" />
-                <div className="cc-stats">
-                    <Stack ml="xs" gap="0">
-                        <Group gap="2px" wrap="nowrap">
-                            <CharacterName name={character.Name} 
-                                element={character.ElementTypes[0]} profession={character.ProfessionType}
-                                level={character.Level} msLevel={character.MindscapeLevel} />
-                            <WeaponEngine weapon={character.Weapon ?? undefined}/>
-                        </Group>
+                <div className="cc-info-bg" />
+                <div className="cc-cell cc-info">
+                    <Group gap="8px" wrap="nowrap">
+                        <CharacterName name={character.Name} 
+                            element={character.ElementTypes[0]} profession={character.ProfessionType}
+                            level={character.Level} msLevel={character.MindscapeLevel} />
+                        <WeaponEngine weapon={character.Weapon ?? undefined}/>
+                    </Group>
+                </div>
+                <div className="cc-vignette" />
+                <div className="cc-cell cc-stats">
+                    {
+                        character.BaseStats.map(s => <Stat key={s.Id ^ character.Id} stat={s} />)
+                    }
+                </div>
+
+                <div className="cc-cell cc-skills">
+                    <Stack gap="6px">
+                        <CoreSkill level={character.CoreSkillEnhancement} />
+                        <Talents talentLevels={character.SkillLevels} />
                     </Stack>
+                </div>
+
+                <div className="cc-discs">
+                    {
+                        character.DriveDisks.map(d => <DriveDisc key={d.Uid} disc={d} />)
+                    }
                 </div>
             </div>
         </Card>
