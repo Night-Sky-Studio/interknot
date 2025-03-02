c336ac141b2814ba43b4bd6ab4be129f37a193a1
LilyStilson <lunamusicoff@gmail.com>
2025-03-03 00:18:58 +0100

    fix: added updating data from dataserver

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,20 +1,23 @@
 import { useParams } from "react-router"
 import { UserHeaderMemorized } from "../components/UserHeader"
-import { Stack } from "@mantine/core"
+import { ActionIcon, Button, Group, Stack } from "@mantine/core"
 import { useLocalStorage } from "@mantine/hooks"
 import CharactersTable from "../components/CharactersTable"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Profile } from "@interknot/types"
-import { useAsync } from "react-use"
+import { useAsyncRetry } from "react-use"
 import { getUser } from "../api/data"
+import { IconReload } from "@tabler/icons-react"
+import Timer from "../components/Timer"
 
 export default function ProfilePage(): React.ReactElement {
     let { uid } = useParams()
 
+    let [needsUpdate, setNeedsUpdate] = useState(false)
     let [savedUsers, setSavedUsers] = useLocalStorage<Profile[]>({ key: "savedUsers" })
 
-    const userState = useAsync(async () => {
-        return await getUser(Number(uid))
+    const userState = useAsyncRetry(async () => {
+        return await getUser(Number(uid), needsUpdate)
     }, [uid])
 
     useEffect(() => {
@@ -26,6 +29,20 @@ export default function ProfilePage(): React.ReactElement {
     return (
         <Stack style={{ minHeight: "150vh" }}>
             {userState.value && <>
+                <Group justify="flex-end">
+                    <Button rightSection={<IconReload />} disabled={needsUpdate} onClick={() => {
+                        setNeedsUpdate(true)
+                        userState.retry()
+                    }}>
+                        <Timer title="Update" endTime={60} isEnabled={needsUpdate} onTimerEnd={() => {
+                            setNeedsUpdate(false)
+                        }} />
+                    </Button>
+                    <ActionIcon style={{ fontFamily: "shicon", fontSize: "1.5rem"}} 
+                        component="a" href={`https://enka.network/zzz/${uid}`} target="_blank">
+                        {""}
+                    </ActionIcon>
+                </Group>
                 <UserHeaderMemorized user={userState.value.Information} />
                 <CharactersTable uid={userState.value.Information.Uid} username={userState.value.Information.Nickname} 
                     characters={userState.value.Characters} />
