bdbf4575853d2abb7a849761d8ef52beb3917198
LilyStilson <lunamusicoff@gmail.com>
2025-03-21 01:13:53 +0100

    fix: adjusted types to work with current backend

--- src/components/LeaderboardButton.tsx
+++ src/components/LeaderboardButton.tsx
@@ -1,15 +1,18 @@
-import { BaseAvatar, WeaponData } from "@interknot/types"
+import { BaseAvatar, BaseWeapon } from "@interknot/types"
 import { Group, UnstyledButton, Image, Title, Stack } from "@mantine/core"
 import "./styles/LeaderboardButton.css"
 import { useSettings } from "./SettingsProvider"
 
 interface ILeaderboardButtonProps {
     agent: BaseAvatar
-    weapon: WeaponData
+    weapon: BaseWeapon
+    name: string
+    rank: number
+    total: number
     type: number
 }
 
-export function LeaderboardButton({ agent, weapon, type }: ILeaderboardButtonProps) {
+export function LeaderboardButton({ agent, weapon, name, rank, total, type }: ILeaderboardButtonProps) {
     const { getLocalString } = useSettings()
     return (
         <UnstyledButton className="lb-button" data-type={type}>
@@ -19,9 +22,9 @@ export function LeaderboardButton({ agent, weapon, type }: ILeaderboardButtonPro
                     <Image src={weapon.ImageUrl} alt={getLocalString(weapon.Name)} />
                 </Group>
                 <Stack gap="0px" className="lb-info">
-                    <Title order={6} fz="12px">{weapon.BuildName}</Title>
-                    <Title order={6} fz="14px" ff="zzz-jp">Top {((weapon.Rank / weapon.Players) * 100).toFixed(1)}%</Title>
-                    <Title order={6} fz="10px" ff="zzz-jp">{weapon.Rank} / {weapon.Players}</Title>
+                    <Title order={6} fz="12px">{name}</Title>
+                    <Title order={6} fz="14px" ff="zzz-jp">Top {((rank / total) * 100).toFixed(1)}%</Title>
+                    <Title order={6} fz="10px" ff="zzz-jp">{rank} / {total}</Title>
                 </Stack>
             </Group>
         </UnstyledButton>
