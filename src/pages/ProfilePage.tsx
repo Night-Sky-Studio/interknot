5dad1b38ce687cb4e55edbd4ff0821692a1c595e
LilyStilson <lunamusicoff@gmail.com>
2025-04-04 18:08:37 +0200

    refactor: fixed indentation in ProfilePage

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -55,42 +55,42 @@ export default function ProfilePage(): React.ReactElement {
             <title>{`${userState.value?.Information.Nickname}'s Profile | Inter-Knot`}</title>
             <meta name="description" content={`${userState.value?.Information.Nickname}'s Profile | Inter-Knot`} />
             <Stack>
-                    <Group justify="flex-end">
-                        <Button rightSection={<IconReload />} disabled={needsUpdate} onClick={() => {
-                            setNeedsUpdate(true)
-                            userState.retry()
-                            leaderboardsState.retry()
-                        }}>
-                            <Timer key={uid} title="Update" isEnabled={needsUpdate}
-                                endTime={userState.value.Ttl === 0 ? 60 : userState.value.Ttl} 
-                                onTimerEnd={() => {
-                                    setNeedsUpdate(false)
-                                }} />
-                        </Button>
-                        <ActionIcon style={{ fontFamily: "shicon", fontSize: "1.5rem"}} 
-                            component="a" href={`https://enka.network/zzz/${uid}`} target="_blank">
-                            {""}
-                        </ActionIcon>
-                    </Group>
-                    <Stack gap="0px" align="center">
-                        <UserHeaderMemorized user={userState.value.Information} showDescription={userState.value.Information.Description !== ""} />
-                        <Collapse in={opened} className="leaderboards" data-open={opened}>
-                            {
-                                leaderboardsState.loading && <Center m="md"><Loader /></Center>
-                            }
-                            {
-                                !leaderboardsState.loading && !leaderboardsState.error && <LeaderboardGridMemorized profile={leaderboardsState.value} characters={userState.value.Characters} />
-                            }
-                            {
-                                leaderboardsState.error && <Center m="md">Failed to load leaderboards</Center>
-                            }
-                        </Collapse>
-                        <Button variant="transparent" className="lb-expand-button" leftSection={opened ? <IconChevronUp /> : <IconChevronDown />} onClick={toggle}>
-                            Leaderboards
-                        </Button>
-                    </Stack>
-                    <CharactersTableMemorized uid={userState.value.Information.Uid} username={userState.value.Information.Nickname} 
-                        characters={userState.value.Characters} lbAgents={leaderboardsState.value?.Agents} />
+                <Group justify="flex-end">
+                    <Button rightSection={<IconReload />} disabled={needsUpdate} onClick={() => {
+                        setNeedsUpdate(true)
+                        userState.retry()
+                        leaderboardsState.retry()
+                    }}>
+                        <Timer key={uid} title="Update" isEnabled={needsUpdate}
+                            endTime={userState.value.Ttl === 0 ? 60 : userState.value.Ttl} 
+                            onTimerEnd={() => {
+                                setNeedsUpdate(false)
+                            }} />
+                    </Button>
+                    <ActionIcon style={{ fontFamily: "shicon", fontSize: "1.5rem"}} 
+                        component="a" href={`https://enka.network/zzz/${uid}`} target="_blank">
+                        {""}
+                    </ActionIcon>
+                </Group>
+                <Stack gap="0px" align="center">
+                    <UserHeaderMemorized user={userState.value.Information} showDescription={userState.value.Information.Description !== ""} />
+                    <Collapse in={opened} className="leaderboards" data-open={opened}>
+                        {
+                            leaderboardsState.loading && <Center m="md"><Loader /></Center>
+                        }
+                        {
+                            !leaderboardsState.loading && !leaderboardsState.error && <LeaderboardGridMemorized profile={leaderboardsState.value} characters={userState.value.Characters} />
+                        }
+                        {
+                            leaderboardsState.error && <Center m="md">Failed to load leaderboards</Center>
+                        }
+                    </Collapse>
+                    <Button variant="transparent" className="lb-expand-button" leftSection={opened ? <IconChevronUp /> : <IconChevronDown />} onClick={toggle}>
+                        Leaderboards
+                    </Button>
+                </Stack>
+                <CharactersTableMemorized uid={userState.value.Information.Uid} username={userState.value.Information.Nickname} 
+                    characters={userState.value.Characters} lbAgents={leaderboardsState.value?.Agents} />
             </Stack>
         </>
     }
