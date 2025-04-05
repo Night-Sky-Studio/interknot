39235762f26007faf677f5a7477de064c670bfb4
LilyStilson <lunamusicoff@gmail.com>
2025-04-05 14:57:46 +0200

    fix: added slight background tint to property cells

--- src/components/PropertyCell.tsx
+++ src/components/PropertyCell.tsx
@@ -4,7 +4,7 @@ import { ZenlessIcon } from "./icons/Icons"
 
 export default function PropertyCell({ prop }: { prop: Property }): React.ReactElement {
     return (
-        <Table.Td>
+        <Table.Td bg="rgba(0 0 0 / 5%)">
             <Group gap="4px" wrap="nowrap">
                 <ZenlessIcon id={prop.Id} size={18} color="white" />
                 <span>{prop.formatted}</span>
