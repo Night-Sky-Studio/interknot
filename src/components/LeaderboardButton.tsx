a0d729dfe6006cc43b8983578b87622f510bf1a7
LilyStilson <lunamusicoff@gmail.com>
2025-04-08 20:31:47 +0200

    fix: added decimal places setting to leaderboard button

--- src/components/LeaderboardButton.tsx
+++ src/components/LeaderboardButton.tsx
@@ -17,9 +17,25 @@ interface ILeaderboardButtonProps {
 }
 
 export function LeaderboardButton({ id, agent, weapon, name, rank, total, type, onClick }: ILeaderboardButtonProps) {
+    // https://stackoverflow.com/a/9462382/10990079
+    const nFormatter = (num: number, digits: number) => {
+        const lookup = [
+            { value: 1, symbol: "" },
+            { value: 1e3, symbol: "k" },
+            { value: 1e6, symbol: "M" },
+            { value: 1e9, symbol: "G" },
+            { value: 1e12, symbol: "T" },
+            { value: 1e15, symbol: "P" },
+            { value: 1e18, symbol: "E" }
+        ]
+        const regexp = /\.0+$|(?<=\.[0-9]*[1-9])0+$/
+        const item = lookup.findLast(item => num >= item.value)
+        return item ? (num / item.value).toFixed(digits).replace(regexp, "").concat(item.symbol) : "0"
+    }
+
     const navigate = useNavigate()
 
-    const { getLocalString } = useSettings()
+    const { decimalPlaces, getLocalString } = useSettings()
 
     const [opened, { toggle, close }] = useDisclosure(false)
 
@@ -37,8 +53,8 @@ export function LeaderboardButton({ id, agent, weapon, name, rank, total, type, 
                         </Group>
                         <Stack gap="0px" className="lb-info">
                             <Title order={6} fz="12px">{name}</Title>
-                            <Title order={6} fz="14px" ff="zzz-jp">Top {((rank / total) * 100).toFixed(1)}%</Title>
-                            <Title order={6} fz="10px" ff="zzz-jp">{rank} / {total}</Title>
+                            <Title order={6} fz="14px" ff="zzz-jp">Top {((rank / total) * 100).toFixed(decimalPlaces)}%</Title>
+                            <Title order={6} fz="10px" ff="zzz-jp">{rank} / {nFormatter(total, 1)}</Title>
                         </Stack>
                     </Group>
                 </UnstyledButton>
