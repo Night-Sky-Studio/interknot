4f39075b26170a43406e3991a10c7ced3c0152cc
LilyStilson <lunamusicoff@gmail.com>
2025-04-08 20:31:00 +0200

    fix: removed units from settings

--- src/components/SettingsProvider.tsx
+++ src/components/SettingsProvider.tsx
@@ -2,21 +2,13 @@ import { createContext, useCallback, useContext, useEffect } from "react"
 import { useLocalStorage } from "@mantine/hooks"
 import { Localizations } from "../localization/Localization"
 
-export enum Unit {
-    CritValue = "CV",
-    RollValue = "RV"
-}
-export const Units: Unit[] = [Unit.CritValue, Unit.RollValue]
-
 type InterknotSettingsBase = {
-    units: Unit,
     decimalPlaces: number,
     language: string,
     lbButtonVariant: number,
 }
 
 type InterknotSettings = InterknotSettingsBase & {
-    setUnits: (value: Unit) => void,
     setDecimalPlaces: (value: number) => void,
     setLanguage: (value: string) => void,
     getLocalString: (value: string) => string,
@@ -24,15 +16,13 @@ type InterknotSettings = InterknotSettingsBase & {
 }
 
 const defaultSettings: InterknotSettingsBase = {
-    units: Unit.CritValue,
-    decimalPlaces: 2,
+    decimalPlaces: 1,
     language: navigator.language.split("-")[0],
     lbButtonVariant: 0
 }
 
 const defaultCallbacks: InterknotSettings = {
     ...defaultSettings,
-    setUnits: () => {},
     setDecimalPlaces: () => {},
     setLanguage: () => {},
     getLocalString: () => "",
@@ -44,7 +34,6 @@ export const SettingsContext = createContext(defaultCallbacks)
 export function SettingsProvider({ children }: { children: React.ReactNode }) {
     const [settings, setSettings] = useLocalStorage<InterknotSettingsBase>({ key: "settings", defaultValue: defaultSettings })
 
-    const setUnits = (units: Unit) => setSettings((prev) => ({ ...prev, units }))
     const setDecimalPlaces = (decimalPlaces: number) => setSettings((prev) => ({ ...prev, decimalPlaces }))
     const setLanguage = (language: string) => setSettings((prev) => ({ ...prev, language }))
     const setLbButtonVariant = (lbButtonVariant: number) => setSettings((prev => ({ ...prev, lbButtonVariant })))
@@ -60,7 +49,6 @@ export function SettingsProvider({ children }: { children: React.ReactNode }) {
     return (
         <SettingsContext.Provider value={{ 
             ...settings, 
-            setUnits, 
             setDecimalPlaces, 
             setLanguage, 
             getLocalString,
