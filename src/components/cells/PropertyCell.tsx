aa55c49bc5017240df6d6c37b4789c8e0eb1f6eb
LilyStilson <lunamusicoff@gmail.com>
2025-04-21 22:23:35 +0200

    feat: added ref and div transformation to property cell

--- src/components/cells/PropertyCell.tsx
+++ src/components/cells/PropertyCell.tsx
@@ -2,9 +2,9 @@ import { Property } from "@interknot/types"
 import { Group, Table } from "@mantine/core"
 import { ZenlessIcon } from "../icons/Icons"
 
-export default function PropertyCell({ prop, className }: { prop: Property, className?: string }): React.ReactElement {
+export default function PropertyCell({ ref, prop, className, useDiv }: { ref?: any, prop: Property, className?: string, useDiv?: boolean }): React.ReactElement {
     return (
-        <Table.Td bg="rgba(0 0 0 / 5%)" className={className}>
+        <Table.Td ref={ref} component={useDiv ? "div" : undefined} bg="rgba(0 0 0 / 5%)" className={className}>
             <Group gap="4px" wrap="nowrap">
                 <ZenlessIcon id={prop.Id} size={18} color="white" />
                 <span>{prop.formatted}</span>
