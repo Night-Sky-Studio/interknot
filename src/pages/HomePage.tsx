cadcf62df7f98113035061512f000abb99ded287
LilyStilson <lunamusicoff@gmail.com>
2025-06-06 14:41:57 +0200

    fix: updated Enka alert

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,12 +6,11 @@ import "./styles/HomePage.css"
 import { useNavigate } from "react-router"
 import { ProfileInfo } from "@interknot/types"
 import { useLocalStorage } from "@mantine/hooks"
-import { IconInfoCircle } from "@tabler/icons-react"
+import { IconAlertTriangle } from "@tabler/icons-react"
 
 export default function HomePage(): React.ReactElement {
     const navigate = useNavigate()
 
-    const [enkaAlertDismissed, setEnkaAlertDismissed] = useLocalStorage({ key: "enkaAlertDismissed", defaultValue: false })
     const [savedUsers, _] = useLocalStorage<ProfileInfo[]>({ key: "savedUsers", defaultValue: [] })
     const [users, setUsers] = useState<ProfileInfo[]>(savedUsers ?? [])
 
@@ -20,20 +19,19 @@ export default function HomePage(): React.ReactElement {
         <section>
             <Title order={1}>Welcome to Inter-Knot</Title>
             <Text>A place for proxies to share their agents' builds and compare their drive discs.</Text>
-            {!enkaAlertDismissed && <>
-                <Space h="lg" />
-                <Alert variant="light" withCloseButton color="blue" data-nosnippet
-                    title="Enka.Network is finally available!" icon={<IconInfoCircle />}
-                    onClose={() => setEnkaAlertDismissed(true)}>
-                    <Text>You can use your own UID to test our website.</Text>
-                    <Text>
-                        Please inform <Text c="blue" component="span"> @lilystilson </Text> on Discord about any encountered bugs. 
-                        Our Discord server is not live! You can 
-                        <Text c="blue" component="a" href="https://discord.gg/hFNheySRQD" target="_blank"> join it </Text>
-                        to chat and report any encountered issues.
-                    </Text>
-                </Alert>
-            </>}
+            <Space h="lg" />
+            <Alert variant="light" color="orange" data-nosnippet
+                title="Enka.Network is on maintenance" icon={<IconAlertTriangle />}>
+                <Text>
+                    Version 2.0 brought significant API changes. 
+                    New profiles addition and profile refreshing is <b>disabled temporarily</b>.
+                </Text>
+                <Text>
+                    Please inform <Text c="blue" component="span"> @lilystilson </Text> on Discord about any encountered bugs. 
+                    You can <Text c="blue" component="a" href="https://discord.gg/hFNheySRQD" target="_blank"> join our Discord server </Text>
+                    to chat and report any encountered issues.
+                </Text>
+            </Alert>
             <PlayerSearch search={(response) => {
                 setUsers(response)
             }} />
