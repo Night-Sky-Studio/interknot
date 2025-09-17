ec0fb6a496dc0a78305678524c58516c14608f08
LilyStilson <lunamusicoff@gmail.com>
2025-09-17 16:18:04 +0200

    feat: added redirect for old profiles to open new url

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "@mantine/notifications/styles.css"
 import { MantineProvider } from "@mantine/core"
 import theme from "./theme"
 import Shell from "./components/Shell"
-import { BrowserRouter, Route, Routes, useParams } from "react-router"
+import { BrowserRouter, Route, Routes, useParams, useLocation, useNavigate, generatePath } from "react-router"
 import ProfilePage from "./pages/ProfilePage"
 import HomePage from "./pages/HomePage"
 import ErrorPage from "./pages/ErrorPage"
@@ -18,10 +18,32 @@ import { ContextMenuProvider } from "mantine-contextmenu"
 // import TestPage from "./pages/TestPage"
 import { Notifications } from "@mantine/notifications"
 import { BackendProvider } from "./components/BackendProvider"
+import { useEffect } from "react"
 
 const ProfilePageWrapper = () => {
     const { uid } = useParams()
     return <ProfilePage key={uid} />
+}
+
+const Redirect = ({ to, replace = true }: { to: string, replace?: boolean }) => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const params = useParams()
+
+    useEffect(() => {
+        // preserve search and hash
+        const pathname = generatePath(to, params)
+        const search = location.search
+        const hash = location.hash
+
+        navigate({
+            pathname: pathname,
+            search,
+            hash,
+        },{ replace })
+    }, [to, replace, navigate, location.search, location.hash, params])
+
+    return null
 }
 
 export default function App() {
@@ -37,8 +59,9 @@ export default function App() {
                                     <Routes>
                                         <Route element={<Shell />}>
                                             <Route index element={<HomePage />} />
-                                            <Route path="/user/:uid" element={<ProfilePageWrapper />} />
                                             <Route path="*" element={<ErrorPage />}/>
+                                            <Route path="/profile/:uid" element={<ProfilePageWrapper />} />
+                                            <Route path="/user/:uid" element={<Redirect to="/profile/:uid" />} />
                                             <Route path="settings" element={<SettingsPage />}/>
                                             <Route path="leaderboards" element={<LeaderboardsPage />} />
                                             <Route path="leaderboards/:id" element={<LeaderboardDetailPage />} />
