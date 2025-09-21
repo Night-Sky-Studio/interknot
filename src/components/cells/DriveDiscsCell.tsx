faea73c3f2b13cb592702bf67b551080745918c9
LilyStilson <lunamusicoff@gmail.com>
2025-09-21 23:45:04 +0200

    fix: detached cells from table component

--- src/components/cells/DriveDiscsCell.tsx
+++ src/components/cells/DriveDiscsCell.tsx
@@ -1,30 +1,28 @@
 import { DriveDiscSet } from "@interknot/types";
-import { Group, Image, Table, TableTdProps, Text, Tooltip } from "@mantine/core";
+import { Group, GroupProps, Image, Text, Tooltip } from "@mantine/core";
 import { useSettings } from "../SettingsProvider";
 
-interface IDriveDiscsCellProps extends TableTdProps {
+interface IDriveDiscsCellProps extends GroupProps {
     sets: DriveDiscSet[]
 }
 
 export default function DriveDiscsCell({ sets, ...props }: IDriveDiscsCellProps): React.ReactElement {
     const { getLocalString } = useSettings()
-    return ( 
-        <Table.Td w="160px" {...props}>
-            <Group gap="8px" wrap="nowrap">
-                {
-                    sets.map(set => {
-                        return (
-                            <Tooltip key={set.Set.Id} label={getLocalString(set.Set.Name)} openDelay={500} 
-                                portalProps={{ reuseTargetNode: true }}>
-                                <Group gap="-14px" align="flex-end" wrap="nowrap">
-                                    <Image src={set.Set.IconUrl} h="32px" /> 
-                                    <Text size="10pt">{set.Count}</Text>
-                                </Group>
-                            </Tooltip>
-                        )
-                    })
-                }
-            </Group>
-        </Table.Td>
+    return (
+        <Group /*w="160px"*/ gap="8px" wrap="nowrap" {...props}>
+            {
+                sets.map(set => {
+                    return (
+                        <Tooltip key={set.Set.Id} label={getLocalString(set.Set.Name)} openDelay={500} 
+                            portalProps={{ reuseTargetNode: true }}>
+                            <Group gap="-14px" align="flex-end" wrap="nowrap">
+                                <Image src={set.Set.IconUrl} h="32px" /> 
+                                <Text size="10pt">{set.Count}</Text>
+                            </Group>
+                        </Tooltip>
+                    )
+                })
+            }
+        </Group>
     )
 }
