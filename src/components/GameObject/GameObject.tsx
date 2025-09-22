1ca90c15d7cc351b87ef0629bccc343d0593d144
LilyStilson <lunamusicoff@gmail.com>
2025-09-22 12:22:11 +0200

    feat: added icons support to GameObject

--- src/components/GameObject/GameObject.tsx
+++ src/components/GameObject/GameObject.tsx
@@ -1,20 +1,23 @@
 import { Group, Image, MantineColor, Text } from "@mantine/core"
 import { useSettings } from "@components/SettingsProvider"
 import "./GameObject.css"
+import { ZenlessIcon } from "../icons/Icons"
 
 export interface IGameObjectProps {
-    img: string
+    img?: string
+    propId?: number
     name: string
     size?: number
     bg?: MantineColor
 }
 
-export default function GameObject({ img, name, size, bg }: IGameObjectProps) {
+export default function GameObject({ img, propId, name, size, bg }: IGameObjectProps) {
     const { getLocalString } = useSettings()
     const s = size ?? 32
     return (
         <Group className="game-object" wrap="nowrap" gap="xs" m="0 0.5rem" p="0 0.5rem 0 0" bg={bg ?? "dark.8"}>
-            <Image src={img} alt={getLocalString(name)} w={`${s}px`} h={`${s}px`} />
+            { img && <Image src={img} alt={getLocalString(name)} w={`${s}px`} h={`${s}px`} /> }
+            { propId && <ZenlessIcon size={s / 1.5} style={{ margin: "0.25rem", marginLeft: "0.5rem" }} id={propId} />}
             <Text>{getLocalString(name)}</Text>
         </Group>
     )
